9d505d661a090d8e6326248add137f2054d1bde3
Eric Erhardt <eric.erhardt@microsoft.com>
2016-05-04 11:11:40 -0500

    Adding copyright headers to all code files

--- Maestro/src/Microsoft.DotNet.Maestro.WebApi/Properties/AssemblyInfo.cs
+++ Maestro/src/Microsoft.DotNet.Maestro.WebApi/Properties/AssemblyInfo.cs
@@ -1,15 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RepoOrchestrator")]
+[assembly: AssemblyTitle("Microsoft.DotNet.Maestro.WebApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RepoOrchestrator")]
+[assembly: AssemblyProduct("Microsoft.DotNet.Maestro")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
