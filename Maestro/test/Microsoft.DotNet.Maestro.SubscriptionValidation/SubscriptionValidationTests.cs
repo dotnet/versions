fbad6fad96cd158b11387ebd6700e054d6a66597
naamunds <naamunds@microsoft.com>
2016-10-05 09:44:04 -0700

    Address PR feedback

--- Maestro/test/Microsoft.DotNet.Maestro.SubscriptionValidation/SubscriptionValidationTests.cs
+++ Maestro/test/Microsoft.DotNet.Maestro.SubscriptionValidation/SubscriptionValidationTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.IO;
 using System.Linq;
 using Microsoft.DotNet.Maestro.Models;
@@ -10,12 +14,6 @@ namespace Microsoft.DotNet.Maestro.SubscriptionValidation
 {
     public class SubscriptionValidationTests
     {
-        [Fact]
-        public void SubscriptionsJsonShouldBeValidJson()
-        {
-            InitializeSubscriptionsModel();
-        }
-
         [Fact]
         public void SubscriptionsJsonShouldHaveValidHandlers()
         {
