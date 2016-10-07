228a2c42fb02cb27ed4274f0be63fc6eebf0b23e
naamunds <naamunds@microsoft.com>
2016-10-07 15:19:01 -0700

    Restore packages in build.cmd

--- Maestro/build.cmd
+++ Maestro/build.cmd
@@ -13,12 +13,22 @@ if not exist "%VS140COMNTOOLS%VsMSBuildCmd.bat" (
   exit /b 1
 )
 
+set cachedNuGet=%LocalAppData%\NuGet\NuGet.exe
 set logFile="%~dp0build.log"
 set solutionPath="%~dp0Microsoft.DotNet.Maestro.sln"
 set buildArgs=/nologo /maxcpucount /nodeReuse:false /v:minimal /clp:Summary /flp:Verbosity=detailed;LogFile=%logFile%;Append %*
 
 REM Add msbuild to the path for the current process.
 call "%VS140COMNTOOLS%\VsMSBuildCmd.bat"
+
+if not exist %cachedNuGet% (
+  echo Downloading latest version of NuGet.exe...
+  if not exist %LocalAppData%\NuGet mkdir %LocalAppData%\NuGet
+  @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%cachedNuGet%'"
+)
+
+echo Restoring packages for %solutionPath%
+%cachedNuGet% restore %solutionPath%
 
 echo Building %solutionPath%
 echo msbuild %solutionPath% %buildArgs%> %logFile%
