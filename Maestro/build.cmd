7e045121caa5278799840b578c6aaf55be33a02c
Matt Mitchell <mmitche@microsoft.com>
2020-04-06 12:57:44 -0700

    Remove netci.groovy and update vmImage (#583)
    * Remove netci.groovy and update vmImage
    
    * Update to VS15
    
    * Use generic vs common tools

--- Maestro/build.cmd
+++ Maestro/build.cmd
@@ -1,16 +1,15 @@
 @echo off
 setlocal
 
-if not exist "%VS140COMNTOOLS%VsMSBuildCmd.bat" (
-  echo In order to run this tool you need either Visual Studio 2015 or
-  echo Microsoft Build Tools 2015 tools installed.
-  echo.
-  echo Visit this page to download either:
-  echo.
-  echo https://www.visualstudio.com/downloads/
-  echo.
-  
-  exit /b 1
+set _VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist %_VSWHERE% (
+  for /f "usebackq tokens=*" %%i in (`%_VSWHERE% -latest -prerelease -property installationPath`) do set _VSCOMNTOOLS=%%i\Common7\Tools
+)
+if not exist "%_VSCOMNTOOLS%" set _VSCOMNTOOLS=%VS150COMNTOOLS%
+if not exist "%_VSCOMNTOOLS%" (
+    echo Error: Visual Studio 2017+ required.
+    echo        Please see https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/developer-guide.md for build instructions.
+    exit /b 1
 )
 
 set nuGetVersion=v3.4.4
@@ -21,7 +20,7 @@ set solutionPath="%~dp0Microsoft.DotNet.Maestro.sln"
 set buildArgs=/nologo /maxcpucount /nodeReuse:false /v:minimal /clp:Summary /flp:Verbosity=detailed;LogFile=%logFile%;Append %*
 
 REM Add msbuild to the path for the current process.
-call "%VS140COMNTOOLS%\VsMSBuildCmd.bat"
+call "%_VSCOMNTOOLS%\VsDevCmd.bat"
 
 if not exist %cachedNuGet% (
   echo Downloading NuGet.exe %nuGetVersion%.
